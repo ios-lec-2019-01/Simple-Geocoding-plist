df4acfd2ba45a3d9d9ab6ff7040253516630bdcd
Jonghyun Kim <jhkim@dit.ac.kr>
2019-10-14 14:25:55 +0900

    Initial Commit

---
+++ SimpleGeocoding02/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SimpleGeocoding02
+//
+//  Created by 김종현 on 2019/10/14.
+//  Copyright © 2019 김종현. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
